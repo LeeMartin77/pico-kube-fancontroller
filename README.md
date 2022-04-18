878ebe8af9910c1bee8b610e0eec3db7716496fe
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-18 09:12:25 +0100

    Added: stdio over USB

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - You need the [pico-sdk](https://github.com/raspberrypi/pico-sdk) on your system, referenced by an env variable `PICO_SDK_PATH`
 - The `make setup` script might not create directories quite correctly - just create `dist` and `build` manually if it's giving grief
 - `make build_binary` builds a binary and drops it into the dist folder, to save rooting it out of the build directory
+- `cat -v < /dev/ttyACM0` will get you the output of the USB connection (probably need to run as root)
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "blink.c"
 #include "enable_pins.c"
@@ -12,6 +13,7 @@ int main() {
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #else
+    stdio_init_all();
     const uint TRANSCEIVER_PIN = 2;
     const int number_of_pins = 1;
     struct PinDefinition pins [number_of_pins];
@@ -19,6 +21,7 @@ int main() {
     pins[0].in_or_out = 1;
     enable_pins(pins, number_of_pins, gpio_init, gpio_set_dir);
     while (true) {
+        printf("Hello from friend!\n");
         blink(TRANSCEIVER_PIN, 2000, 250, gpio_put, sleep_ms);
     }
 #endif
