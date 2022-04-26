5a83b00ac391ab836e55a13bb02383c437a9af96
Lee Martin <lee.martin@pliminus.co.uk>
2022-04-26 21:53:10 +0100

    Moving toward an even curve, plus testing all percentages.

--- src/fan_drive.c
+++ src/fan_drive.c
@@ -4,9 +4,19 @@ typedef void (*fn_gpio_put)(unsigned int,bool);
 
 typedef void (*fn_sleep_ms)(unsigned int);
 
-void pulse(unsigned int pin, unsigned int on_duration, unsigned int off_duration, fn_gpio_put gpio_put, fn_sleep_ms sleep_ms) {
-  gpio_put(pin, false);
-  sleep_ms(off_duration);
+void pulse(unsigned int pin, unsigned int on_percentage, unsigned int off_percentage, fn_gpio_put gpio_put, fn_sleep_ms sleep_ms) {
+  assert(on_percentage + off_percentage == 100);
+  while (on_percentage + off_percentage > 0) {
+    if(off_percentage) {
+      gpio_put(pin, false);
+      sleep_ms(1);
+      off_percentage--;
+    }
+    if(on_percentage) {
+      gpio_put(pin, true);
+      sleep_ms(1);
+      on_percentage--;
+    }
+  }
   gpio_put(pin, true);
-  sleep_ms(on_duration);
 }
--- src/fan_drive.test.c
+++ src/fan_drive.test.c
@@ -4,7 +4,6 @@
 #include "fan_drive.c"
 
 void test_fan_drive() {
-
   unsigned int expected_pin_number = 12;
 
   unsigned int total_sleep_duration = 0;
@@ -12,6 +11,9 @@ void test_fan_drive() {
   sleep_durations[0] = 0;
   sleep_durations[1] = 0;
   bool pin_on = 0;
+
+  unsigned int expected_on_duration = 50;
+  unsigned int expected_off_duration = 50;
 
   void fake_gpio_put(unsigned int pin_number, bool value) {
     assert(expected_pin_number == pin_number);
@@ -22,16 +24,26 @@ void test_fan_drive() {
     total_sleep_duration = total_sleep_duration + duration;
     const int pin_index = pin_on ? 1 : 0;
     sleep_durations[pin_index] = sleep_durations[pin_index] + duration;
+    // Ensure that we only have more than ms off if we have to
+    if(!pin_on && expected_off_duration <= ((expected_off_duration + expected_on_duration)/2)) {
+      assert(duration < 2);
+    }
   }
+  for (int i = 0; i <= 100; i++) {
+    total_sleep_duration = 0;
+    sleep_durations[0] = 0;
+    sleep_durations[1] = 0;
 
-  unsigned int expected_on_duration = 3849;
-  unsigned int expected_off_duration = 7583;
-  
-  pulse(expected_pin_number, expected_on_duration, expected_off_duration, fake_gpio_put, fake_sleep_ms);
+    expected_on_duration = i;
+    expected_off_duration = 100 - i;
+    pin_on = 0;
 
-  assert(total_sleep_duration == (expected_on_duration + expected_off_duration));
-  assert(sleep_durations[1] == expected_on_duration);
-  assert(sleep_durations[0] == expected_off_duration);
-  // MAke sure pin is pulled high at end always
-  assert(pin_on);
+    pulse(expected_pin_number, expected_on_duration, expected_off_duration, fake_gpio_put, fake_sleep_ms);
+
+    assert(total_sleep_duration == (expected_on_duration + expected_off_duration));
+    assert(sleep_durations[1] == expected_on_duration);
+    assert(sleep_durations[0] == expected_off_duration);
+    // MAke sure pin is pulled high at end always
+    assert(pin_on);
+  }
 }
